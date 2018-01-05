7d001dc89cc7501c3c6326d8612b5f247efeeade
Nyi Nyi Htun Lwin <nnhl.mgk@gmail.com>
2018-01-05 19:03:25 +0630

    Project Skeleton.

---
+++ app/src/main/java/projects/nyinyihtunlwin/foodplaces/FoodPlacesApp.java
@@ -0,0 +1,14 @@
+package projects.nyinyihtunlwin.foodplaces;
+
+import android.app.Application;
+
+/**
+ * Created by Dell on 1/5/2018.
+ */
+
+public class FoodPlacesApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
---
+++ app/src/main/java/projects/nyinyihtunlwin/foodplaces/activities/BaseActivity.java
@@ -0,0 +1,10 @@
+package projects.nyinyihtunlwin.foodplaces.activities;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Dell on 1/5/2018.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+}
