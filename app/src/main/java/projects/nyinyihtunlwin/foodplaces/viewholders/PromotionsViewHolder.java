43e9b17bb65d44fa072824ee3fe621ff69095983
Nyi Nyi Htun Lwin <nnhl.mgk@gmail.com>
2018-01-18 23:19:06 +0630

    Data binding updated.

--- app/src/main/java/projects/nyinyihtunlwin/foodplaces/viewholders/PromotionsViewHolder.java
+++ app/src/main/java/projects/nyinyihtunlwin/foodplaces/viewholders/PromotionsViewHolder.java
@@ -1,7 +1,12 @@
 package projects.nyinyihtunlwin.foodplaces.viewholders;
 
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import projects.nyinyihtunlwin.foodplaces.R;
 import projects.nyinyihtunlwin.foodplaces.data.vo.PromotionVO;
 
 /**
@@ -10,14 +15,28 @@ import projects.nyinyihtunlwin.foodplaces.data.vo.PromotionVO;
 
 public class PromotionsViewHolder extends BaseViewHolder<PromotionVO> {
 
+    @BindView(R.id.tv_promotion_title)
+    TextView tvPromotionTitle;
+
+    @BindView(R.id.tv_exclusive)
+    TextView tvExclusive;
+
+    @BindView(R.id.tv_promotion_until)
+    TextView tvPromotionUntil;
+
+    @BindView(R.id.iv_promotion_image)
+    ImageView ivPromotionImage;
+
 
     public PromotionsViewHolder(View itemView) {
         super(itemView);
+        ButterKnife.bind(this, itemView);
     }
 
     @Override
     public void setData(PromotionVO data) {
-
+        tvPromotionTitle.setText(data.getPromotionTitle());
+        tvPromotionUntil.setText(data.getPromotionUntil());
     }
 
     @Override
